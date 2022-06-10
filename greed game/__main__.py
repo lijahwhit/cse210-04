fca6f30fd63d1f5933ef254f99c3b7edef4e228e
NotBitcoinCEO <96089214+NotBitcoinCEO@users.noreply.github.com>
2022-06-09 18:51:20 -0700

    changed

--- greed game/__main__.py
+++ greed game/__main__.py
@@ -43,7 +43,7 @@ def main():
     position = Point(x, y)
 
     robot = Actor()
-    robot.set_text("####")
+    robot.set_text("#")
     robot.set_font_size(FONT_SIZE)
     robot.set_color(WHITE)
     robot.set_position(position)
